46366102a3912f2bea9285a25bae671dcb6ebcb0
Pavel Aslanov <asl.pavel@gmail.com>
2018-10-10 12:03:41 +0100

    [cleanup] remove acidently commited `site-autoload.el` file

--- site-autoload.el
+++
@@ -1,61 +0,0 @@
-;;; site-autoload.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "palette-gruvbox-dark-theme" "palette-gruvbox-dark-theme.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from palette-gruvbox-dark-theme.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "palette-gruvbox-dark-theme" '("palette-gruvbox-dark")))
-
-;;;***
-
-;;;### (autoloads nil "palette-gruvbox-light-theme" "palette-gruvbox-light-theme.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from palette-gruvbox-light-theme.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "palette-gruvbox-light-theme" '("palette-gruvbox-light")))
-
-;;;***
-
-;;;### (autoloads nil "palette-satori-theme" "palette-satori-theme.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from palette-satori-theme.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "palette-satori-theme" '("palette-satori")))
-
-;;;***
-
-;;;### (autoloads nil "palette-subatomic-theme" "palette-subatomic-theme.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from palette-subatomic-theme.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "palette-subatomic-theme" '("palette-subatomic")))
-
-;;;***
-
-;;;### (autoloads nil "palette-theme-lib" "palette-theme-lib.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from palette-theme-lib.el
-
-(autoload 'palette-theme-gen "palette-theme-lib" "\
-Generate palette theme with NAME from COLORS, with CONTRAST, and PRIME color.
-
-NAME     : deftheme symbol name
-COLORS   : ((color-name . color) ...)
-CONTRAST : (:hard|:normal|:soft)
-PRIME    : one color-name from COLORS
-
-\(fn NAME COLORS &optional CONTRAST PRIME)" nil t)
-
-;;;***
-
-(provide 'site-autoload)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; site-autoload.el ends here
